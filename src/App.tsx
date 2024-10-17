a7efc69e8a84cc242995e31090ac841474c1f40a
Mariana Antonia <maridev889@gmail.com>
2024-10-17 11:28:59 -0300

    tipando o codigo

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,39 @@
 import { useEffect, useState } from 'react'
 import { getApi_Search } from './services/requestApi'
 
+interface SearchResult {
+  Poster: string;
+  Title: string;
+  Year: string;
+  Type: string;
+}
+
+interface ApiData {
+  Search: SearchResult[];
+}
+
 function App() {
-  const [dados, setDados] = useState()
-  const [searchValue, setSearchValue] = useState('harry potter') // Valor inicial padrão
+  const [dados, setDados] = useState<ApiData | null>(null);
+  const [searchValue, setSearchValue] = useState('harry potter');
 
   useEffect(() => {
     async function getData() {
       try {
-        const data = await getApi_Search(searchValue)
-        setDados(data)
+        const data = await getApi_Search(searchValue);
+        setDados(data);
         // console.log(data)
       } catch (error) {
         console.log(`Não foi possivel completar a requisição, ERROR ${error}`)
       }
     }
-    getData()
-  }, [searchValue])
+    getData();
+  }, [searchValue]);
 
-  const handleSearch = (event) => {
-    setSearchValue(event.target.value)
-  }
+  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchValue(event.target.value);
+  };
+
+
 
   return (
     <div>
@@ -30,12 +43,12 @@ function App() {
         type="search"
         name="pesquisa"
         id="pesquisa"
-        placeholder='Digite o nome'
+        placeholder="Digite o nome"
         value={searchValue}
         onChange={handleSearch}
       />
       <ul>
-        {dados?.Search?.map((e, i) => (
+        {dados?.Search?.map((e: SearchResult, i: number) => (
           <li key={i}>
             {/* {console.log(e)} */}
             <img src={e.Poster} alt={e.Title} />
--- src/services/requestApi.ts
+++ src/services/requestApi.ts
@@ -3,8 +3,8 @@ import { BASE_URL, ID } from "../utils/variaveis"
 
 const CHAVE_API_KEY = import.meta.env.VITE_CHAVEAPI
 
-export async function getApi_Search(SEARCH_VALUE) {
-    const URL_SEARCH = `${BASE_URL}/?s=${SEARCH_VALUE}&page=1&apikey=${CHAVE_API_KEY}`
+export async function getApi_Search(searchValue) {
+    const URL_SEARCH = `${BASE_URL}/?s=${searchValue}&page=1&apikey=${CHAVE_API_KEY}`
     const response = await axios.get(URL_SEARCH)
     const data = await response.data
     return data
