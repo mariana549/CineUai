f901d4a972526f13c2977cdf5b6a327b0577f90c
Mariana Antonia <maridev889@gmail.com>
2024-10-25 11:51:10 -0300

    atualizações no  home

--- src/page/home.tsx
+++ src/page/home.tsx
@@ -9,15 +9,13 @@ import { InputSearch } from "../components/search";
 export function Home() {
   const [dados, setDados] = useState<ApiData | null>(null);
   const [searchValue, setSearchValue] = useState(localStorage.getItem('searchValue') || "harry potter");
+  const [numPageAtual, setNumPageAtual] = useState(parseInt(localStorage.getItem('numPageAtual') || '1', 10));
   const [erroMinLength, setErrorMinLength] = useState('');
-
+  const [notFound, setNotFound] = useState<string | null>(null);
   const [favorites, setFavorites] = useState<Record<string, boolean>>(() => {
     const savedFavorites = localStorage.getItem('favorites');
     return savedFavorites ? JSON.parse(savedFavorites) : {};
   });
-
-  const [notFound, setNotFound] = useState<string | null>(null);
-  const [numPageAtual, setNumPageAtual] = useState(parseInt(localStorage.getItem('numPageAtual') || '1', 10));
 
   useEffect(() => {
     const useStateDados = {
@@ -27,30 +25,33 @@ export function Home() {
       setNotFound: setNotFound,
       setErrorMinLength: setErrorMinLength,
     }
+    
     getData(useStateDados);
+
+    localStorage.setItem('searchValue', searchValue);
+    localStorage.setItem('numPageAtual', numPageAtual.toString());
   }, [searchValue, numPageAtual]);
 
   useEffect(() => {
     // Salva os favoritos no localStorage sempre que mudar
     localStorage.setItem('favorites', JSON.stringify(favorites));
-    // localStorage.setItem('id', JSON.stringify(id));
   }, [favorites]);
 
   const toggleFavorite = (title: string, id: string) => {
     setFavorites((prevFavorites) => {
-      const updatedFavorites = { 
-        ...prevFavorites, 
-        [id]: { title, isFavorite: !prevFavorites[id]?.isFavorite || false } 
-      }; 
-      
+      const updatedFavorites = {
+        ...prevFavorites,
+        [id]: { title, isFavorite: !prevFavorites[id]?.isFavorite || false }
+      };
+
       // Remove o item do localStorage se não for mais favorito
       if (!updatedFavorites[id].isFavorite) {
         delete updatedFavorites[id];
       }
-      
+
       localStorage.setItem('favorites', JSON.stringify(updatedFavorites));
       return updatedFavorites;
-    });    
+    });
   };
 
   const numTotalResults = dados?.totalResults || 0;
