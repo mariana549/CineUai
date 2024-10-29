648463ae1425e3e43e8b868b51e7af2927a45087
Mariana Antonia <maridev889@gmail.com>
2024-10-29 15:55:43 -0300

    melhorando a tipagem

--- src/services/requestApi.ts
+++ src/services/requestApi.ts
@@ -3,7 +3,7 @@ import { BASE_URL } from "../utils/variaveis"
 
 const CHAVE_API_KEY = import.meta.env.VITE_CHAVEAPI
 
-export async function getApi_Search(searchValue: string | number, numPageAtual: number, type: string | undefined, year: number | undefined) {
+export async function getApi_Search(searchValue: string | number, numPageAtual: number, type: string | undefined, year: string | number | undefined) {
     const URL_SEARCH = `${BASE_URL}/?s=${searchValue}&page=${numPageAtual}&type=${type}&y=${year}&apikey=${CHAVE_API_KEY}`
     const response = await axios.get(URL_SEARCH)
     const data = await response.data
