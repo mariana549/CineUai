05258b24665dfc6d3858da41e003d1eeef8ee7a5
Mariana Antonia <maridev889@gmail.com>
2024-11-04 13:33:31 -0300

    feat: add novos recursos e colocando o home em uma pasta

---
+++ src/page/home/home.tsx
@@ -0,0 +1,35 @@
+import { MainCards } from "../../components/main";
+import { HeaderCards } from "../../components/header";
+
+import { clearAllFavorites } from "../../functions/favorites/clearAllFavorites";
+import { useFavorites } from "../../hooks/useFavorites";
+import { SavedFavoritesList } from "../../components/buttons/savedFavoritesList";
+
+import { ClearButton, Container, H4 } from "../../globalStyled";
+import { Pheader } from "../../components/header/headerStyled";
+import { Nav } from "../../components/nav/nav";
+import { Footer } from "../../components/footer";
+
+export const Home = () => {
+  const { setFavorites } = useFavorites();
+
+  return (
+    <Container>
+ 
+      <HeaderCards>
+        <Pheader>Enter endless entertainment! Discover all about your movies and series now.</Pheader>
+        <Nav/>
+      </HeaderCards>
+
+      <MainCards>
+        <ClearButton onClick={() => clearAllFavorites(setFavorites)}>
+        Delete All Favorites
+        </ClearButton>
+        <H4>Full list of favorites</H4>
+        <SavedFavoritesList />
+      </MainCards>
+
+      <Footer />
+    </Container>
+  )
+}
