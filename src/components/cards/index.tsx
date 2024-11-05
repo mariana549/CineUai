de570bb30a53279aff91765c1ab2776b946aaa2c
Mariana Antonia <124698724+mariana549@users.noreply.github.com>
2024-11-05 14:56:23 -0300

    style: :art: mudanças que não afetam o codigo

--- src/components/cards/index.tsx
+++ src/components/cards/index.tsx
@@ -1,32 +1,44 @@
-import { CardContent, CardStled, ImgCardStyled, MoreInfoButton, SpanCardStyled, TituloCardStyled } from './cardsStyled';
-import { Link } from 'react-router-dom';
-import { useData } from '../../hooks/useData';
-import { usePage } from '../../hooks/usePage';
-import { ButtonFavorite } from '../buttons/buttonFavorite/buttonfavorite';
-import { cardProps } from '../../utils/types/interfaces';
+import {
+  CardContent,
+  CardStled,
+  ImgCardStyled,
+  MoreInfoButton,
+  SpanCardStyled,
+  TituloCardStyled,
+} from "./cardsStyled";
+import { Link } from "react-router-dom";
+import { useData } from "../../hooks/useData";
+import { usePage } from "../../hooks/usePage";
+import { ButtonFavorite } from "../buttons/buttonFavorite/buttonfavorite";
+import { cardProps } from "../../utils/types/interfaces";
 
 import semFoto from "../../assets/images/semfoto.png";
 
-export const Cards = ({ dados }:cardProps) => {
+export const Cards = ({ dados }: cardProps) => {
   const { searchValue } = useData();
   const { numPageAtual } = usePage();
 
   const handleLinkClick = () => {
-    localStorage.setItem('searchValue', searchValue);
-    localStorage.setItem('numPageAtual', numPageAtual.toString());
+    localStorage.setItem("searchValue", searchValue);
+    localStorage.setItem("numPageAtual", numPageAtual.toString());
   };
 
   return (
     <>
       {dados?.map((e, i) => (
         <CardStled key={i}>
-          <ImgCardStyled src={e.Poster === "N/A" ? semFoto : e.Poster} alt={e.Title} />
-          <ButtonFavorite favoriteDados={e} bg={"rgba(0, 0, 0, 0.5)"}/> 
+          <ImgCardStyled
+            src={e.Poster === "N/A" ? semFoto : e.Poster}
+            alt={e.Title}
+          />
+          <ButtonFavorite favoriteDados={e} bg={"rgba(0, 0, 0, 0.5)"} />
           <CardContent>
             <TituloCardStyled>{e.Title}</TituloCardStyled>
-            <SpanCardStyled>{e.Year} {e.Type}</SpanCardStyled>
+            <SpanCardStyled>
+              {e.Year} {e.Type}
+            </SpanCardStyled>
             <Link
-              to={`/${e.Type}/${e.imdbID}/${e.Title.replace(/\s+/g, '_')}`}
+              to={`/${e.Type}/${e.imdbID}/${e.Title.replace(/\s+/g, "_")}`}
               onClick={handleLinkClick}
             >
               <MoreInfoButton>see more</MoreInfoButton>
--- src/components/nav/nav.tsx
+++ src/components/nav/nav.tsx
@@ -6,42 +6,39 @@ import { Box, SpanErrorInput } from "../../globalStyled";
 import { StyledInput, StyledOption, StyledSelect } from "./navStyled";
 
 export function Nav() {
-    const { searchValue, erroMinLength, notFound, year, setYear, setType } = useData();
+  const { searchValue, erroMinLength, notFound, year, setYear, setType } =
+    useData();
 
-    const isValidYear = year && (typeof year === "number");
-    const validationMessage = isValidYear && year <= 1889 ? "Please enter a valid year." : "";
+  const isValidYear = year && typeof year === "number";
+  const validationMessage =
+    isValidYear && year <= 1889 ? "Please enter a valid year." : "";
 
-    return (
-        <nav>
-            <InputSearch />
-            <SpanErrorInput>
-                {searchValue && searchValue.length <= 2 ? erroMinLength : ""}
-            </SpanErrorInput>
+  return (
+    <nav>
+      <InputSearch />
+      <SpanErrorInput>
+        {searchValue && searchValue.length <= 2 ? erroMinLength : ""}
+      </SpanErrorInput>
 
-            <Box>
-                <StyledSelect
-                    name="types"
-                    onChange={type => getTypes(type, setType)}
-                >
-                    <StyledOption value="">all</StyledOption>
-                    <StyledOption value="movie">movie</StyledOption>
-                    <StyledOption value="series">series</StyledOption>
-                    <StyledOption value="game">game</StyledOption>
-                </StyledSelect>
+      <Box>
+        <StyledSelect name="types" onChange={(type) => getTypes(type, setType)}>
+          <StyledOption value="">all</StyledOption>
+          <StyledOption value="movie">movie</StyledOption>
+          <StyledOption value="series">series</StyledOption>
+          <StyledOption value="game">game</StyledOption>
+        </StyledSelect>
 
-                <StyledInput
-                    type="number"
-                    name="ano"
-                    min={1890}
-                    max={2080}
-                    placeholder="YYYY"
-                    onChange={year => getYear(year, setYear)}
-                />
-            </Box>
-            <SpanErrorInput>
-                {year ? notFound : ""}
-            </SpanErrorInput>
-            <SpanErrorInput>{validationMessage}</SpanErrorInput>
-        </nav>
-    )
+        <StyledInput
+          type="number"
+          name="ano"
+          min={1890}
+          max={2080}
+          placeholder="YYYY"
+          onChange={(year) => getYear(year, setYear)}
+        />
+      </Box>
+      <SpanErrorInput>{year ? notFound : ""}</SpanErrorInput>
+      <SpanErrorInput>{validationMessage}</SpanErrorInput>
+    </nav>
+  );
 }
