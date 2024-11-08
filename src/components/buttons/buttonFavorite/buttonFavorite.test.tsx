807e2952e8c88e2d8430886f20ad83ffcf5c4432
Mariana Antonia <124698724+mariana549@users.noreply.github.com>
2024-11-08 11:19:27 -0300

    test: :white_check_mark: testes atualizados e funcionado corretamente

--- src/components/buttons/buttonFavorite/buttonFavorite.test.tsx
+++ src/components/buttons/buttonFavorite/buttonFavorite.test.tsx
@@ -1,18 +1,82 @@
-import { render } from '@testing-library/react'
-import { ButtonFavorite } from './buttonfavorite'
+import { fireEvent, render, screen } from "@testing-library/react";
+import { ButtonFavorite } from "./buttonfavorite";
+import { toggleFavorite } from "../../../functions/favorites/toggleFavorite";
+import { useFavorites } from "../../../hooks/useFavorites";
 
-  const mockFavoriteDados = {
-    Title: "Harry Potter and the Deathly Hallows: Part 2",
-    imdbID: "tt1201607",
-  };
+jest.mock("../../../hooks/useFavorites");
+jest.mock("../../../functions/favorites/toggleFavorite");
 
+const mockFavoriteDados = {
+  Title: "Harry Potter and the Deathly Hallows: Part 2",
+  imdbID: "tt1201607",
+};
+
+const mockFavorites: Record<string, { title: string; isFavorite: boolean }> =
+  {};
 
 describe("buttonFavorite", () => {
-  it("deve renderizar o component", () => {
-    const { getByAltText } = render(
+  beforeEach(() => {
+    localStorage.clear();
+
+    (useFavorites as jest.Mock).mockReturnValue({
+      favorites: {},
+      setFavorites: jest.fn(),
+    });
+  });
+
+  it("deve renderizar o componente", () => {
+    render(
       <ButtonFavorite favoriteDados={mockFavoriteDados} bg={"transparent"} />
     );
-    const buttonFavorite = getByAltText("favorite")
-    expect(buttonFavorite).toBeInTheDocument()
-  })
-})
+    const buttonFavorite = screen.getByAltText("favorite");
+
+    expect(buttonFavorite).toBeInTheDocument();
+  });
+
+  it("deve chamar toggleFavorite ao clicar no botão e retornar dados", () => {
+    const setFavoritesMock = jest.fn();
+    const title = "Harry Potter and the Deathly Hallows: Part 2";
+    const id = "tt1201607";
+    const initialFavorites = {
+      [id]: { title, isFavorite: true },
+    };
+
+    setFavoritesMock.mockImplementation(
+      (
+        callback: (prevFavorites: typeof mockFavorites) => typeof mockFavorites
+      ) => {
+        const newFavorites = callback(initialFavorites);
+        expect(newFavorites[id]).toEqual({ title, isFavorite: true });
+        return newFavorites;
+      }
+    );
+
+    (useFavorites as jest.Mock).mockReturnValue({
+      favorites: initialFavorites,
+      setFavorites: setFavoritesMock,
+    });
+
+    render(<ButtonFavorite favoriteDados={mockFavoriteDados} bg="blue" />);
+    fireEvent.click(screen.getByAltText("favorite"));
+
+    expect(toggleFavorite).toHaveBeenCalledWith(title, id, setFavoritesMock);
+  });
+
+  it("deve chamar toggleFavorite ao clicar no botão e retornar vazio", () => {
+    const setFavoritesMock = jest.fn(() => {
+      return {};
+    });
+
+    (useFavorites as jest.Mock).mockReturnValue({
+      favorites: {},
+      setFavorites: setFavoritesMock,
+    });
+
+    render(
+      <ButtonFavorite favoriteDados={{ Title: "", imdbID: "" }} bg="blue" />
+    );
+    fireEvent.click(screen.getByAltText("favorite"));
+
+    expect(toggleFavorite).toHaveBeenCalledWith("", "", setFavoritesMock);
+  });
+});
