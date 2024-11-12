5633877c78741915252de388a65546a6e20f39a4
Mariana Antonia <124698724+mariana549@users.noreply.github.com>
2024-11-12 11:08:39 -0300

    test: :white_check_mark: componente Footer testado

--- src/components/footer/footer.test.tsx
+++ src/components/footer/footer.test.tsx
@@ -1,11 +1,37 @@
-// import { render } from "@testing-library/react";
-// import { Footer } from ".";
+import { render, screen } from "@testing-library/react";
+import { Footer } from ".";
+import { BrowserRouter as Router } from "react-router-dom";
 
 describe("Footer", () => {
   it("deve renderizar o component", () => {
-    // const { getByRole } = render(<Footer />);
-    // const footer = getByRole("Mariana Antonia");
-    // expect(footer).toBeInTheDocument();
-    expect(true).toBe(true);
+    render(
+      <Router>
+        <Footer />
+      </Router>
+    );
+    const footer = screen.getByText(/Creating by/);
+    expect(footer).toBeInTheDocument();
   });
+
+  it("deve renderizar o link de Rede social coretamente", () => {
+    render(
+      <Router>
+        <Footer />
+      </Router>
+    );
+    const link = screen.getByText(/Mariana Antonia/);
+    expect(link).toBeInTheDocument();
+    expect(link).toHaveAttribute("href", "https://github.com/mariana549");
+  })
+
+  it("deve renderizar o link de navegação corretamente", () => {
+    render(
+      <Router>
+        <Footer />
+      </Router>
+    );
+    const link = screen.getByText(/Home/);
+    expect(link).toBeInTheDocument();
+    expect(link.closest("a")).toHaveAttribute("href", '/');
+  })
 });
