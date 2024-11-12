dde0aa3bed7a529b37bdbf581cf9989a3655bb25
Mariana Antonia <124698724+mariana549@users.noreply.github.com>
2024-11-12 15:56:55 -0300

    test: :white_check_mark: add test header

--- src/components/header/header.test.tsx
+++ src/components/header/header.test.tsx
@@ -1,5 +1,43 @@
+import { render, screen } from "@testing-library/react";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Header } from ".";
+import logo from "../../assets/images/logo.png";
 
+describe("Header", () => {
+  it("deve renderizar o componete", () => {
+    render(
+      <Router>
+        <Header />
+      </Router>
+    );
 
-describe("HeaderCards", () => {
-  
-})
+    const header = screen.getByText(/Cine Uai/);
+    expect(header).toBeInTheDocument();
+  });
+
+  it("deve exibir a imagem logo e o texto cine uai corretamente", () => {
+    render(
+      <Router>
+        <Header />
+      </Router>
+    );
+
+    const imgLogo = screen.getByAltText("logo");
+    expect(imgLogo).toBeInTheDocument();
+    expect(imgLogo).toHaveAttribute("src", logo);
+
+    const logoText = screen.getByText("Cine Uai");
+    expect(logoText).toBeInTheDocument();
+  });
+
+  it("deve renderizar os children corretamente", () => {
+    render(
+      <Header>
+        <div>Child Element</div>
+      </Header>
+    );
+
+    const childElement = screen.getByText('Child Element'); 
+    expect(childElement).toBeInTheDocument();
+  });
+});
